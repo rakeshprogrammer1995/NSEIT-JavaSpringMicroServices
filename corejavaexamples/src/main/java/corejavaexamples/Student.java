17361ced5060f00a692a156c8c83a20e42f5e706
praveeniiht <64822037+praveeniiht@users.noreply.github.com>
2022-02-21 17:29:53 +0530

    Add files via upload

--- corejavaexamples/src/main/java/corejavaexamples/Student.java
+++ corejavaexamples/src/main/java/corejavaexamples/Student.java
@@ -1,8 +1,42 @@
 package corejavaexamples;
 
 public class Student {
-int htno;
-
 	
+	private int hallTicketNumber;
+    private String name;
+    private String branch;
+    private String college;
+    
+	public int getHallTicketNumber() {
+		return hallTicketNumber;
+	}
+	public void setHallTicketNumber(int hallTicketNumber) {
+		this.hallTicketNumber = hallTicketNumber;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public String getCollege() {
+		return college;
+	}
+	public void setCollege(String college) {
+		this.college = college;
+	}
+	
+	public void getStudentDetails(){
+		// code 
+	}
+	public void displayStudentDetails(){
+		// code
+	}
 
 }
---
+++ corejavaexamples/src/main/java/corejavaexamples/TestStudent.java
@@ -0,0 +1,17 @@
+package corejavaexamples;
+
+public class TestStudent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student stu1= new Student();
+		stu1.setHallTicketNumber(1001);
+		stu1.setName("praveen");
+		stu1.setBranch("cse");
+		stu1.setCollege("JNTU");
+		System.out.println(stu1.getBranch());
+
+	}
+
+}
